84a57432337e340cdccbf9d6d72630821ffaaa35
Nikolay Andreev <bakasaru@list.ru>
2022-02-05 20:28:49 +0300

    Add nice index page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
 
 
 @app.route('/about')
