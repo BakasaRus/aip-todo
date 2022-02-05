9524195be77f40165bfd05f48d0231ee5b0b9c34
Nikolay Andreev <bakasaru@list.ru>
2022-02-05 19:54:38 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/about')
+def about():
+    return 'About me'
+
+
+if __name__ == '__main__':
+    app.run()
